c6e104fe1cb4cb47b150a7461294f4e3a880344f
Diego Moreno Montes <e2diego@gmail.com>
2017-09-24 10:11:57 +0200

    Initial Commit

---
+++ GPSCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSCalculator
+//
+//  Created by Diego on 24/9/17.
+//  Copyright © 2017 Diego Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
